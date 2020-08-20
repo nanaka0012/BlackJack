51dbaa9e63d5d6819425007ebefe6b7a9a5ff41e
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-08-20 12:52:27 +0900

    ADD: Deck class

---
+++ BlackJack/Card.cs
@@ -0,0 +1,107 @@
+﻿using Altseed2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public enum Mark
+    {
+        Diamond,
+        Spade,
+        Heart,
+        Club,
+    }
+
+    public class Card : SpriteNode
+    {
+        private int no;
+        private Mark mark;
+
+        public Mark Mark
+        {
+            get => mark;
+            set
+            {
+                mark = value;
+                UpdateTexture();
+            }
+        }
+
+        /// <summary>
+        /// トランプの数値
+        /// </summary>
+        public int No
+        {
+            get => no;
+            set
+            {
+                if (value >= 1 && value <= 13)
+                {
+                    no = value;
+                    UpdateTexture();
+                }
+            }
+        }
+
+        /// <summary>
+        /// トランプの表示
+        /// </summary>
+        public string NoString
+        {
+            get
+            {
+                switch (No)
+                {
+                    case 1:
+                        return "A";
+                    case 11:
+                        return "J";
+                    case 12:
+                        return "Q";
+                    case 13:
+                        return "K";
+                }
+
+                return No.ToString();
+            }
+        }
+
+        public Card()
+        {
+            no = 1;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Card card)
+                return card.Mark == Mark && card.No == No;
+            return false;
+        }
+
+        private void UpdateTexture()
+        {
+            string prefix;
+            switch (Mark)
+            {
+                case Mark.Diamond:
+                    prefix = "d";
+                    break;
+                case Mark.Spade:
+                    prefix = "s";
+                    break;
+                case Mark.Heart:
+                    prefix = "h";
+                    break;
+                case Mark.Club:
+                    prefix = "c";
+                    break;
+                default:
+                    prefix = null;
+                    break;
+            }
+
+            Texture = Texture2D.Load($"resources/tramps/{prefix}_{No}.png");
+        }
+    }
+}
---
+++ BlackJack/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BlackJack
+{
+    public class Deck
+    {
+        public Stack<Card> Stack { get; set; }
+
+        public Deck()
+        {
+            Stack = new Stack<Card>();
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (Mark mark in Enum.GetValues(typeof(Mark)))
+                {
+                    foreach (int no in Enumerable.Range(1, 13))
+                    {
+                        Stack.Push(new Card() { No = no, Mark = mark });
+                    }
+                }
+            }
+            Shuffle(Stack);
+        }
+
+        public void Shuffle<T>(Stack<T> stack)
+        {
+            var rnd = new Random();
+            var values = stack.ToArray();
+            stack.Clear();
+            foreach (var value in values.OrderBy(x => rnd.Next()))
+                stack.Push(value);
+        }
+
+        public Card Draw()
+        {
+            return Stack.Pop();
+        }
+    }
+}
